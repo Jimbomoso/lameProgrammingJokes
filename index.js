fd2572ea57f0b22d0f6b4673462a4b6ce32613ce
jimbomoso <jimbomoso@gmail.com>
2020-12-11 12:43:43 -0500

    Creates function to get joke and console logs joke

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 // creates event listeners
 document.getElementById('newJokeBtn').addEventListener('click', getNewJoke);
-document.getElementById('punchlineBtn').addEventListener('click', getPunchline);
+// document.getElementById('punchlineBtn').addEventListener('click', getPunchline);
 
+// getJoke function
+async function getNewJoke() {
+    // fetches joke
+    const jokePromise = await fetch
+    ('https://official-joke-api.appspot.com/jokes/programming/random');
+
+    // Consumes json data
+    const joke = await jokePromise.json();
+
+    console.log(joke);
+}
+
+getNewJoke();
